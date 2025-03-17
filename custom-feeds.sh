580bbd0dca1f022e67379d42df1d899ba5720dc6
0xffff07c <licoln.lee.us@outlook.com>
2025-03-17 23:33:22 +0800

    Update custom-feeds.sh

--- custom-feeds.sh
+++ custom-feeds.sh
@@ -2,5 +2,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' $BUILD_ROOT/feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >> $BUILD_ROOT/feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> $BUILD_ROOT/feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> $BUILD_ROOT/feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> $BUILD_ROOT/feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >> $BUILD_ROOT/feeds.conf.default
